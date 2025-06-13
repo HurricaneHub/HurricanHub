29d2e2c8b348169d6ad47c5b8bec62df397e7ea3
Anil Kumar Raut <95755697+anilkumarraut@users.noreply.github.com>
2025-06-12 21:23:53 -0400

    Springboot backend config by Anil

---
+++ backend/springboot/src/main/java/com/example/hurricanhub/HurricanHubApplication.java
@@ -0,0 +1,11 @@
+package com.example.hurricanhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurricanHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HurricanHubApplication.class, args);
+    }
+}
